7b08b14b8605660c7d30f3be0da65ac04a300cd6
Stacy-G-97 <stacygathoni30@gmail.com>
2025-09-01 08:55:08 +0300

    Updated DataService.ts

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { BarChart, Home, Package, TrendingUp, Users, Crown } from 'lucide-react-native';
+import { ChartBar as BarChart, Chrome as Home, Package, TrendingUp, Users, Crown } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
